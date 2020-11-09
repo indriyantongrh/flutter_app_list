4e2b6630fc224c3a97c66117f75abff74eb1a93c
INDRIYANTO NUGROHO <masyantoya@gmail.com>
2020-11-09 15:30:52 +0700

    Initial Comit Fetch List data

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,32 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_app_list/model/modelresponse.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Application Flutter'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -46,58 +45,145 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- var nameCity=[
-   "Semarang",
-   "Pekalongan",
-   "Batang",
-   "Cirebon",
-   "Tegal",
-   "Banyumas",
-   "Surakarta",
-   "Yogyakarta",
-   "Bandung",
-   "Ciledug",
-   "Blodar",
-   "Jakarta Barat",
-   "Jakarta Selatan",
-   "Jakarta Pusat",
-   "Jakarta Timur",
-   "Sumedang",
-   "Bogor",
-   "Salatiga",
-   "Ungaran",
-   "Pati",
-   "Jepara",
-   "Purwodadi",
 
- ];
+  List<modelresponse> _modelresponse = List<modelresponse>();
+
+  Future<List<modelresponse>> fetchData() async{
+    final response =
+    await http.get("https://jsonplaceholder.typicode.com/users");
+
+    var datas = List<modelresponse>();
+
+    if (response.statusCode == 200) {
+      var datasJson = json.decode(response.body);
+      for(var datasJson in datasJson){
+        datas.add(modelresponse.fromJson(datasJson));
+      }
+    }
+    return datas;
+  }
 
   @override
   Widget build(BuildContext context) {
+    fetchData().then((value) {
+      _modelresponse.addAll(value);
+    });
+
 
     return Scaffold(
       appBar: AppBar(
 
         title: Text(
-          "Applocation Listview"
+          "Application Listview"
         ),
       ),
 
       body: ListView.separated(
-
         separatorBuilder: (context, index){
           return Divider(
               color: Colors.grey,
           );
           },
         itemBuilder: (context, index){
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(nameCity[index]),
+
+          return Card(
+            child: Padding(
+            padding: const EdgeInsets.only(top: 30.0, bottom: 32, left: 16.0, right: 16.0),
+              child: Column(
+
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    _modelresponse[index].name,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    "Username: "+_modelresponse[index].username,
+                    style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.normal
+                    ),
+                  ),
+                  Text(
+                    "Email: "+_modelresponse[index].email,
+                    style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.normal
+                    ),
+                  ),
+                  Text(
+                    "Phone: "+_modelresponse[index].phone,
+                    style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.normal
+                    ),
+                  ),
+                  Text(
+                    "Website: "+ _modelresponse[index].website,
+                    style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.normal
+                    ),
+                  ),
+
+                  Text(
+                    "Detail Alamat",
+                    style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+
+                  Text(
+                    "Street: "+_modelresponse[index].address.suite+", "+ _modelresponse[index].address.street+", "+_modelresponse[index].address.city+"\n Zip Code: "+_modelresponse[index].address.zipcode,
+                    style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.normal
+                    ),
+                  ),
+                  Text(
+                      "Detail Company",
+                    style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+
+                  Text(
+                    "Name Company: "+_modelresponse[index].company.name,
+
+                    style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.normal
+                    ),
+                  ),
+
+                  Text(
+                    "Catchphrase: "+_modelresponse[index].company.catchPhrase,
+                    style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.normal
+                    ),
+                  ),
+                  Text(
+                    "BS: "+_modelresponse[index].company.bs,
+                    style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.normal
+                    ),
+                  )
+                ],
+              ),
+            ),
           );
         },
-        itemCount: nameCity.length,
+        itemCount: _modelresponse.length,
       ),
     );
   }
+
+
 }
